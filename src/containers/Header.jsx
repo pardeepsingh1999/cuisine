8c4e689665b884c127b0b1bb9170363b8157c1ef
Pardeep Singh <pardeep@logic-square.com>
2023-09-28 13:21:53 +0530

    menu zindex fix

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -68,7 +68,7 @@ const Navbar = () => {
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-sm dropdown-content mt-3 z-[999] p-2 shadow bg-base-100 rounded-box w-52"
           >
             {routes.map((each) => (
               <li key={each.title}>
@@ -151,7 +151,7 @@ const Navbar = () => {
 
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              className="menu menu-sm dropdown-content mt-3 z-[999] p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
                 <NavLink
